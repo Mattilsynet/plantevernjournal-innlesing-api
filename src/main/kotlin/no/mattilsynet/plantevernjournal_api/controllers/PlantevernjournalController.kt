be667ac5c08f3d3b1de7f37d221cfcf3a796b5ab
Beate Undeberget <beate.undeberget@mattilsynet.no>
2025-10-02 11:50:39 +0200

    PLANTE-1490: Lagt til body for å lese inn frø og formeringsmateriale (#10)

--- src/main/kotlin/no/mattilsynet/plantevernjournal_api/controllers/PlantevernjournalController.kt
+++ src/main/kotlin/no/mattilsynet/plantevernjournal_api/controllers/PlantevernjournalController.kt
@@ -1,13 +1,22 @@
 package no.mattilsynet.plantevernjournal_api.controllers
 
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import no.mattilsynet.plantevernjournal_api.controllers.models.FroeEllerFormeringsMatrialeDto
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("plantevernjournal/innlesing/v1")
+@Tag(
+    description = "Endepunkter for å lese inn plantevernjournaler",
+    name = "Innlesing plantevernjournal",
+)
 class PlantevernjournalController {
 
     @GetMapping
@@ -15,8 +24,13 @@ class PlantevernjournalController {
         return ResponseEntity.ok().build()
     }
 
-    @PostMapping
-    fun postPlantevernjournal(): ResponseEntity<Unit> {
+    @PostMapping("/formeringsmateriale")
+    fun postFroeEllerFormeringsmateriale(
+        @Parameter(
+            name = "froeEllerFormeringsMatrialeDto",
+            description = "Data for å plantevernjournal for formeringsmateriale eller frø"
+        ) @Valid @RequestBody froeEllerFormeringsMatrialeDto: FroeEllerFormeringsMatrialeDto,
+    ): ResponseEntity<Unit> {
         return ResponseEntity.ok().build()
     }
 }
--- src/main/kotlin/no/mattilsynet/plantevernjournal_api/shared/Bruksomraade.kt
+++ src/main/kotlin/no/mattilsynet/plantevernjournal_api/shared/Bruksomraade.kt
@@ -3,8 +3,8 @@ package no.mattilsynet.plantevernjournal_api.shared
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(
-    description = "Bruksområde for bruk av plantevernmidlers",
+    description = "Bruksområde for bruk av plantevernmidler",
 )
-enum class Bruksomraade(description: String) {
-    JORDBRUK(description = "Jordbruk"),
+enum class Bruksomraade(beskrivelse: String) {
+    JORDBRUK(beskrivelse = "Jordbruk"),
 }
