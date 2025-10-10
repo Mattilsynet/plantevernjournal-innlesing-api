af6ffb91511c58f1e85d2cd3239d99efb826b3da
17480 <beate.undeberget@mattilsynet.no>
2025-10-10 09:56:18 +0200

    PLANTE-1492: Fikset testklasse

--- src/test/kotlin/no/mattilsynet/plantevernjournal/api/PlantevernjournalApiApplicationTests.kt
+++ src/test/kotlin/no/mattilsynet/plantevernjournal/api/PlantevernjournalApiApplicationTests.kt
@@ -1,12 +1,37 @@
 package no.mattilsynet.plantevernjournal.api
 
+import no.mattilsynet.fisk.libs.virtualnats.VirtualNats
+import no.mattilsynet.plantevernjournal.api.nats.consumers.FroeEllerFormeringsMaterialePushConsumer
+import no.mattilsynet.plantevernjournal.api.nats.consumers.InnendoersPushConsumer
+import no.mattilsynet.plantevernjournal.api.nats.consumers.UtendoersPushConsumer
+import no.mattilsynet.plantevernjournal.api.services.NatsService
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @SpringBootTest
+@kotlin.uuid.ExperimentalUuidApi
+@kotlinx.serialization.ExperimentalSerializationApi
+@Suppress("UnusedPrivateMember")
 class PlantevernjournalApiApplicationTests {
 
+    @MockitoBean
+    private lateinit var froeEllerFormeringsMaterialePushConsumer: FroeEllerFormeringsMaterialePushConsumer
+
+    @MockitoBean
+    private lateinit var innendoersPushConsumer: InnendoersPushConsumer
+
+    @MockitoBean
+    private lateinit var utendoersPushConsumer: UtendoersPushConsumer
+
+    @MockitoBean
+    private lateinit var natsService: NatsService
+
+    @MockitoBean
+    private lateinit var nats: VirtualNats
+
 	@Test
+    @Suppress("EmptyFunctionBlock")
 	fun contextLoads() {
 	}
 
