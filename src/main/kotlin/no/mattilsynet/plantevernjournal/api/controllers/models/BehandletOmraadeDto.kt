84c50aa691a172df2532f5abfc1b202fa13a2a02
Beate Undeberget <beate.undeberget@mattilsynet.no>
2026-07-09 11:00:21 +0200

    PLANTE-1894: Rettet encoding for behandletOmraade, sånn at de leses og persisteres som JsonElement

--- src/main/kotlin/no/mattilsynet/plantevernjournal/api/controllers/models/BehandletOmraadeDto.kt
+++ src/main/kotlin/no/mattilsynet/plantevernjournal/api/controllers/models/BehandletOmraadeDto.kt
@@ -1,6 +1,11 @@
 package no.mattilsynet.plantevernjournal.api.controllers.models
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 import org.wololo.geojson.FeatureCollection
 
-fun FeatureCollection.toBehandledeOmraader() : List<String> =
-    features.map { it.toString() }
+fun FeatureCollection.toBehandledeOmraader() : List<JsonElement> =
+    features.map { feature ->
+        Json.parseToJsonElement(jacksonObjectMapper().writeValueAsString(feature))
+    }
--- src/main/kotlin/no/mattilsynet/plantevernjournal/api/domain/InnendoersBruk.kt
+++ src/main/kotlin/no/mattilsynet/plantevernjournal/api/domain/InnendoersBruk.kt
@@ -1,6 +1,7 @@
 package no.mattilsynet.plantevernjournal.api.domain
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
 import no.mattilsynet.plantevernjournal.api.controllers.models.BehandletVekstDto
 import no.mattilsynet.plantevernjournal.api.controllers.models.PlantevernmiddelDto
 import no.mattilsynet.plantevernjournal.api.controllers.models.responses.InnendoersBrukResponsDto
@@ -22,7 +23,7 @@ data class InnendoersBruk(
     @Serializable(with = LocalDateSerializer::class)
     val behandletDato: LocalDate,
 
-    val behandledeOmraader: List<String>?,
+    val behandledeOmraader: List<JsonElement>?,
 
     val bruksomraade: Bruksomraade,
 
